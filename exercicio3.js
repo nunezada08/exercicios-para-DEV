f49dfb94a4f6d27cd01ac44194196ac664df8360
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:06:14 -0300

    feat: resolução do exercicio 3

---
+++ exercicio3.js
@@ -0,0 +1,14 @@
+let producao = [
+  "mousepad",
+  "placa de vídeo",
+  "fonte de alimentação",
+  "HD externo",
+  "notebook gamer",
+  "roteador"
+]
+
+let produtoRemovido
+
+produtoRemovido = producao.pop()
+
+console.log(producao)
