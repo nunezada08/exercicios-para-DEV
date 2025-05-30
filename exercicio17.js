7f40758c520a75ebd2a0391c2af0d21bfb070ff9
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:53:52 -0300

    feat: resolução do exercicio 17

---
+++ exercicio17.js
@@ -0,0 +1,15 @@
+let usuariosSistemaA = [
+  "aline.souza",
+  "maria.ferreira",
+  "carlos.lima",
+  "ana.martins"
+]
+
+let usuariosSistemaB = [
+  "joao.silva",
+  "paulo.pereira",
+  "fernanda.alves",
+  "marcelo.cardoso"
+]
+
+console.log("Todos os usuarios do sistema:", usuariosSistemaA.concat(usuariosSistemaB));
