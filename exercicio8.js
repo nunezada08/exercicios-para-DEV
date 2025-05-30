5d0f9a2da3b8af116dcb826e647993fb9677962f
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:23:39 -0300

    feat: resolução do exercicio 8

---
+++ exercicio8.js
@@ -0,0 +1,13 @@
+let chamada = [
+  "Carlos",
+  "Fernanda",
+  "João",
+  "Mariana",
+  "Lucas",
+  "Camila"
+]
+
+// adicionando dois elementos no comeco da array
+chamada.unshift("Beatriz","Rafaela")
+
+console.log(chamada)
