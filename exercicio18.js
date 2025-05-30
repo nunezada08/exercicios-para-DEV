6cb875ec251b0fd0ec3a6deea3e29451779f08f8
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:55:21 -0300

    feat: resolução do exercicio 18

---
+++ exercicio18.js
@@ -0,0 +1,15 @@
+let hardware = [
+  "processador i7",
+  "placa-mãe",
+  "memória RAM 16GB",
+  "SSD 1TB"
+]
+
+let perifericos = [
+  "teclado gamer",
+  "mouse ótico",
+  "webcam HD",
+  "fone com microfone"
+]
+
+console.log("Hardware e perifericos:", hardware.concat(perifericos));
