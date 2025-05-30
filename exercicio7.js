b71eb7c7ca1d4883c12d684657b4670a1ec95aab
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:20:40 -0300

    feat: resolução do exercicio 7

---
+++ exercicio7.js
@@ -0,0 +1,12 @@
+let filaAtendimento = [
+  "cliente 003",
+  "cliente 004",
+  "cliente 005",
+  "cliente 006",
+  "cliente 007"
+]
+
+// adicionando dois elementos ao inicio da array
+filaAtendimento.unshift("cliente VIP1","cliente VIP2")
+
+console.log(filaAtendimento)
