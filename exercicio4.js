8c356540a10dae0a2450a3401253c3d878e0cae2
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:10:23 -0300

    feat: resolução do exercicio 4

--- exercicio4.js
+++ exercicio4.js
@@ -1,3 +1,10 @@
 let inscritos = [
   "Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"
 ]
+
+let alunoRetirado
+
+// removendo ultimo item da array
+alunoRetirado = inscritos.pop()
+
+console.log("inscritos:",inscritos)
