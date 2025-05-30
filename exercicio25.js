bdc24d1b5c84388783a753714092203a1a90017d
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 15:23:30 -0300

    feat: resolução do exercicio 25

---
+++ exercicio25.js
@@ -0,0 +1,13 @@
+let atualizacoes = [
+    "Backup diário concluído",
+    "Servidor reiniciado",
+    "Nova versão implantada",
+    "Logs exportados",
+    "Firewall atualizado",
+    "Monitoramento ativado",
+    "Banco de dados otimizado",
+    "Script de segurança executado"
+]
+  
+let atualizacoesMaisRecentes = atualizacoes.reverse()
+console.log(atualizacoesMaisRecentes)
