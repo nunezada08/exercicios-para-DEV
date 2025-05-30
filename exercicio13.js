015bad0b32e4101ac6bf2ebc9b5f51a96cfd07d2
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:39:45 -0300

    feat: resolução do exercicio 13

---
+++ exercicio13.js
@@ -0,0 +1,13 @@
+let diasFaltas = [
+  "Carlos – Segunda",
+  "Carlos – Terça",
+  "Carlos – Quarta",
+  "Carlos – Quinta",
+  "Carlos – Sexta",
+  "Carlos – Segunda (semana 2)",
+  "Carlos – Terça (semana 2)",
+  "Carlos – Quarta (semana 2)"
+]
+
+
+console.log("Primeira semana de ferias: ",diasFaltas.slice(0, 5))
