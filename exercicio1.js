8f421e81c080ded32b951cfe3d9e6efd82fc9d5f
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:02:06 -0300

    feat: resolução do exercicio 1

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+let promocoes = [
+  "teclado mecânico",
+  "mouse gamer",
+  "monitor ultrawide",
+  "cadeira ergonômica",
+  "webcam HD",
+  "fone bluetooth",
+  "notebook i5",
+  "hub USB"
+]
+
+promocoes.push("SSD 1TB", "mesa digitalizadora")
+
+console.log(promocoes)
