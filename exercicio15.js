a0c296b51c228f5f2cad655c14f46a6d518ff35e
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:46:19 -0300

    feat: resolução do exercicio 15

---
+++ exercicio15.js
@@ -0,0 +1,12 @@
+let menuNavegacao = [
+  "Início",
+  "Serviços",
+  "Portfólio",
+  "Blog",
+  "Contato",
+  "Login",
+  "Área do cliente"
+]
+
+//removendo o item 2 e 3 da array
+console.log(menuNavegacao.splice(1, 2))
