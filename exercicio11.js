83a1313cf8fc72263b4cf2fff00a9c0c435ba58a
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:32:31 -0300

    feat: resolução do exercicio 11

---
+++ exercicio11.js
@@ -0,0 +1,11 @@
+let ingredientes = [
+  "farinha de trigo",
+  "fermento",
+  "leite",
+  "ovos",
+  "açúcar",
+  "manteiga"
+]
+
+// transformando a array em string
+console.log(ingredientes.join(', '))
