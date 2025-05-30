39ebd8ccf8ef9b74cb5734d6601f67f88ea2579e
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:15:24 -0300

    feat: resolução do exercicio 5

---
+++ exercicio5.js
@@ -0,0 +1,13 @@
+let importados = [
+  "carregador portátil",
+  "cabo HDMI",
+  "leitor biométrico",
+  "controle sem fio",
+  "webcam 4K"
+]
+let importadoRemovido
+
+//removendo o pirmeiro item da array
+importados.shift()
+
+console.log("itens do carregamento:",importados)
