adf5dda0aff7036952aa29ec253c2436bb04e012
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:57:35 -0300

    feat: resolução do exercicio 19

---
+++ exercicio19.js
@@ -0,0 +1,14 @@
+let codigos = [
+  "abc123",
+  "def456",
+  "ghi789",
+  "jkl012",
+  "mno345",
+  "pqr678",
+  "stu901",
+  "vwx234"
+]
+
+let codigosMaiusculo = codigos.map(item => item.toUpperCase());
+
+console.log("Nova lista em maiusculo:",codigosMaiusculo)
