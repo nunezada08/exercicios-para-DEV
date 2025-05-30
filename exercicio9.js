6c7136a08894e754847de4f4c7926509da6c1a45
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:26:51 -0300

    feat: resolução do exercicio 9

---
+++ exercicio9.js
@@ -0,0 +1,14 @@
+let presenca = [
+  "João Lima",
+  "Carla Nunes",
+  "Amanda Souza",
+  "Bruno Ribeiro",
+  "Fernanda Sales"
+]
+let pessoa = "Amanda Souza"
+
+if (presenca.includes(pessoa)) {
+    console.log(`${pessoa} estava presente`)
+}else{
+    console.log(`${pessoa} não estava presente`)
+}
