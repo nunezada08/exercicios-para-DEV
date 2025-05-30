12db34fb42b9afb7ad5a1b2012295e7c34c7d9fb
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:36:28 -0300

    feat: resolução do exercicio 12

---
+++ exercicio12.js
@@ -0,0 +1,10 @@
+let palavrasChave = [
+  "desenvolvimento",
+  "frontend",
+  "javascript",
+  "css",
+  "react"
+]
+
+//transformando em string e separando por traços
+console.log(palavrasChave.join('-'))
