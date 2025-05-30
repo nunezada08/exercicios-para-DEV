7254d6d48c68587df1bd16d2beb9c14101192294
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:17:11 -0300

    feat: resolução do exercicio 6

---
+++ exercicio6.js
@@ -0,0 +1,12 @@
+let mensagens = [
+  "Propaganda indevida",
+  "Bom dia, posso ajudar?",
+  "Preciso de suporte técnico",
+  "Claro, qual o problema?",
+  "Erro ao acessar conta"
+]
+
+//removendo primeiro item da array
+mensagens.shift()
+
+console.log(mensagens)
