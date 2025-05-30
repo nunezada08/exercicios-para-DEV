8c73ab2802e15446cf2ffb799101dcb97cffc69b
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 14:45:05 -0300

    feat: resolução do exercicio 23

---
+++ exercicio23.js
@@ -0,0 +1,15 @@
+let candidatos = [
+    "Marcos",
+    "Aline",
+    "Carla",
+    "João",
+    "Fernanda",
+    "Lucas",
+    "Bruno",
+    "Isabela"
+]
+
+// organizando a lista em ordem alfabetica
+let organizada = candidatos.sort()
+
+console.log(organizada)
