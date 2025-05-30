dabf4a1b16bd7c394d404d00791e50a53c436232
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 15:29:28 -0300

    feat: resolução do exercicio 26

---
+++ exercicio26.js
@@ -0,0 +1,15 @@
+let historico = [
+    "login",
+    "painel",
+    "relatórios",
+    "detalhes do cliente",
+    "editar dados",
+    "configurações",
+    "logout"
+]
+
+// invertendo a lista
+let historicoInvertido = historico.reverse()
+
+console.log(historico)
+  
