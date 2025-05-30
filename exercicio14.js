f63028d4db3ab8160a0d7288e3978fe1531d627e
nunezada08 <vitor.nunes3013@gmail.com>
2025-05-30 11:42:58 -0300

    feat: resolução do exercicio 14

---
+++ exercicio14.js
@@ -0,0 +1,12 @@
+let artigosBlog = [
+  "Como usar Docker com Node.js",
+  "5 erros comuns em JavaScript",
+  "Introdução ao TypeScript",
+  "Clean Code na prática",
+  "Design Patterns úteis",
+  "Refatoração de código",
+  "Boas práticas de API REST",
+  "Segurança em aplicações web"
+]
+
+console.log("4 artigos mais recentes:",artigosBlog.slice(0, 4))
